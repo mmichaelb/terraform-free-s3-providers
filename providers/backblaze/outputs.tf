93e85ded28e6719f4ac5d04e4c97d285e87ca635
mmichaelb <mmichaelb@protonmail.com>
2023-09-27 01:43:44 +0200

    adapt standard attribute access type

--- providers/backblaze/outputs.tf
+++ providers/backblaze/outputs.tf
@@ -3,19 +3,19 @@ output "profile_name" {
 }
 
 output "endpoint_url" {
-  value = trimprefix(lookup(data.b2_account_info.general, "s3_api_url"), "https://")
+  value = trimprefix(data.b2_account_info.general.s3_api_url, "https://")
 }
 
 output "region" {
-  value = regex("s3.([\\w-]+).backblazeb2.com", lookup(data.b2_account_info.general, "s3_api_url"))[0]
+  value = regex("s3.([\\w-]+).backblazeb2.com", data.b2_account_info.general.s3_api_url)[0]
 }
 
 output "access_key_id" {
-  value = lookup(b2_application_key.access, "application_key_id")
+  value = b2_application_key.access.application_key_id
 }
 
 output "secret_access_key" {
-  value = lookup(b2_application_key.access, "application_key")
+  value = b2_application_key.access.application_key
 }
 
 output "bucket_name" {
