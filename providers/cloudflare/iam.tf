6d28f7584467464919a09934da8884546a324cc8
mmichaelb <mmichaelb@protonmail.com>
2025-09-16 21:16:44 +0200

    adapt cloudflare provider v5

--- providers/cloudflare/iam.tf
+++ providers/cloudflare/iam.tf
@@ -1,27 +1,46 @@
-data "cloudflare_api_token_permission_groups" "all" {}
+data "cloudflare_api_token_permission_groups_list" "all" {}
+
+locals {
+  workers_r2_storage_bucket_item_write = data.cloudflare_api_token_permission_groups_list.all.result[index(
+      data.cloudflare_api_token_permission_groups_list.all.result.*.name,
+      "Workers R2 Storage Bucket Item Write"
+    )]
+  workers_r2_storage_bucket_item_read = data.cloudflare_api_token_permission_groups_list.all.result[index(
+      data.cloudflare_api_token_permission_groups_list.all.result.*.name,
+      "Workers R2 Storage Bucket Item Read"
+    )]
+}
 
 resource "cloudflare_api_token" "access" {
   name = "${var.bucket_name}-access"
-  policy {
+  policies = [{
+    effect = "allow"
     permission_groups = [
-      data.cloudflare_api_token_permission_groups.all.r2["Workers R2 Storage Bucket Item Write"]
+      {
+        id = local.workers_r2_storage_bucket_item_write.id
+      }
     ]
     resources = {
       "com.cloudflare.edge.r2.bucket.${var.account_id}_default_${var.bucket_name}" = "*"
     }
-  }
+  }]
 }
 
 resource "cloudflare_api_token" "management" {
   count = var.add_management_key ? 1 : 0
   name  = "${var.bucket_name}-management"
-  policy {
+  policies = [{
+    effect = "allow"
     permission_groups = [
-      data.cloudflare_api_token_permission_groups.all.r2["Workers R2 Storage Bucket Item Write"],
-      data.cloudflare_api_token_permission_groups.all.r2["Workers R2 Storage Bucket Item Read"]
+      {
+        id = local.workers_r2_storage_bucket_item_write.id
+      },
+      {
+        id = local.workers_r2_storage_bucket_item_read.id
+      }
     ]
     resources = {
       "com.cloudflare.edge.r2.bucket.${var.account_id}_default_${var.bucket_name}" = "*"
     }
-  }
+  }]
 }
--- providers/cloudflare/test/versions.tf
+++ providers/cloudflare/test/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "4.13.0"
+      version = "5.10.0"
     }
     sops = {
       source  = "carlpett/sops"
